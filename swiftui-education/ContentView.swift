a8e96020fa1c95acc87082c0babe7da6f22e385d
hira22 <m.hiroaka22@gmail.com>
2019-09-03 20:34:46 +0900

    Initial Commit

---
+++ swiftui-education/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  swiftui-education
+//
+//  Created by 平岡真之 on 2019/09/03.
+//  Copyright © 2019 hiraoka. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ swiftui-educationTests/swiftui_educationTests.swift
@@ -0,0 +1,34 @@
+//
+//  swiftui_educationTests.swift
+//  swiftui-educationTests
+//
+//  Created by 平岡真之 on 2019/09/03.
+//  Copyright © 2019 hiraoka. All rights reserved.
+//
+
+import XCTest
+@testable import swiftui_education
+
+class swiftui_educationTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
